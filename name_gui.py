c6873232e1cbea1a6dd9d025e3ea475745d8e58d
AM-ssfs <menscad@ssfs.org>
2021-11-20 11:16:05 -0500

    ui stuff

--- name_gui.py
+++ name_gui.py
@@ -11,6 +11,9 @@ name = tk.StringVar()
 user_input = tk.Entry(root, textvariable=name)
 user_input.grid(row=1, column=1)
 
+hello = tk.Label(root, text="hello " + name.get())
+hello.grid(row=2, column=1)
+
 say_hello = tk.Button(root, text="say hello", command=hi)
 say_hello.grid(row=3, column=1)
 tk.mainloop()
