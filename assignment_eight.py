b92a404248745f80c8abd3427d102e6695ea99e3
AM-ssfs <menscad@ssfs.org>
2021-12-06 09:53:48 -0500

    calc

--- assignment_eight.py
+++ assignment_eight.py
@@ -1,28 +1,113 @@
+import tkinter
 import tkinter as tk
 
-root=tk.Tk()
+root = tk.Tk()
 root.title("calculator")
 
 """
 vars
 """
 
-equation = tk.StringVar
+equation = tkinter.StringVar()
+equation.set("")
 
 """
 functions
 """
 
-def press_number():
-    t=equation.get
-    t+="7"
-    text_field.set(t)
+
+def press_1():
+    n = equation.get()
+    n += "1"
+    equation.set(n)
+def press_2():
+    n = equation.get()
+    n += "2"
+    equation.set(n)
+def press_3():
+    n = equation.get()
+    n += "3"
+    equation.set(n)
+def press_4():
+    n = equation.get()
+    n += "4"
+    equation.set(n)
+def press_5():
+    n = equation.get()
+    n += "5"
+    equation.set(n)
+def press_6():
+    n = equation.get()
+    n += "6"
+    equation.set(n)
+def press_7():
+    n = equation.get()
+    n += "7"
+    equation.set(n)
+def press_8():
+    n = equation.get()
+    n += "8"
+    equation.set(n)
+def press_9():
+    n = equation.get()
+    n += "9"
+    equation.set(n)
+def press_0():
+    n = equation.get()
+    n += "0"
+    equation.set(n)
+
+def press_add():
+    n = equation.get()
+    n += "+"
+    equation.set(n)
+def press_subtract():
+    n = equation.get()
+    n += "-"
+    equation.set(n)
+def press_multiply():
+    n = equation.get()
+    n += "*"
+    equation.set(n)
+def press_divide():
+    n = equation.get()
+    n += "/"
+    equation.set(n)
+
+def press_exponent():
+    n = equation.get()
+    n += "^"
+    equation.set(n)
+
+def press_clear():
+    equation.set("")
+
+def press_solve():
+    n = equation.get()
+    if "^" in n:
+        pos = int(n.index("^"))
+        if n[pos+1] == "0":
+            equation.set("1")
+        else:
+            before = n[:pos]
+            after = int(n[pos+1:])
+            n = before
+            while after > 1:
+                n += "*"+before
+                after -= 1
+            equation.set(n)
+
+    if "√" in n:
+        pass
+    n = eval(n)
+    equation.set(n)
+
 
 """
 entry
 """
 
-text_field = tk.Entry(root, textVariable=equation)
+text_field = tk.Entry(root, textvariable=equation)
 text_field.grid(row=1, column=1)
 
 
@@ -33,61 +118,62 @@ numbers 0-9
 frame1 = tk.Frame(root)
 frame1.grid(row=2, column=1, columnspan=2, pady=10)
 
-button1 = tk.Button(frame1, text="1")
+button1 = tk.Button(frame1, text="1", command=press_1)
 button1.grid(row=2, column=1)
-button2 = tk.Button(frame1, text="2")
+button2 = tk.Button(frame1, text="2", command=press_2)
 button2.grid(row=2, column=2)
-button3 = tk.Button(frame1, text="3")
+button3 = tk.Button(frame1, text="3", command=press_3)
 button3.grid(row=2, column=3)
 
 frame2 = tk.Frame(root)
 frame2.grid(row=3, column=1, columnspan=2, pady=10)
 
-button4 = tk.Button(frame2, text="4")
+button4 = tk.Button(frame2, text="4", command=press_4)
 button4.grid(row=3, column=1)
-button5 = tk.Button(frame2, text="5")
+button5 = tk.Button(frame2, text="5", command=press_5)
 button5.grid(row=3, column=2)
-button6 = tk.Button(frame2, text="6")
+button6 = tk.Button(frame2, text="6", command=press_6)
 button6.grid(row=3, column=3)
 
 frame3 = tk.Frame(root)
 frame3.grid(row=4, column=1, columnspan=2, pady=10)
 
-button7= tk.Button(frame3, text="7")
+button7= tk.Button(frame3, text="7", command=press_7)
 button7.grid(row=4, column=1)
-button8= tk.Button(frame3, text="8")
+button8= tk.Button(frame3, text="8", command=press_8)
 button8.grid(row=4, column=2)
-button9= tk.Button(frame3, text="9")
+button9= tk.Button(frame3, text="9", command=press_9)
 button9.grid(row=4, column=3)
 
 frame4 = tk.Frame(root)
 frame4.grid(row=5, column=1, columnspan=2, pady=10)
 
-button0 = tk.Button(frame4, text="0")
+button0 = tk.Button(frame4, text="0", command=press_0)
 button0.grid(row=5, column=2)
 
 """
-functions
+operators
 """
 
-clear = tk.Button(root, text="Clear")
+clear = tk.Button(root, text="Clear", command=press_clear)
 clear.grid(row=1, column=2)
 
-add = tk.Button(root, text="+")
+add = tk.Button(root, text="+", command=press_add)
 add.grid(row=1, column=3)
-subtract = tk.Button(root, text="-")
+subtract = tk.Button(root, text="-", command=press_subtract)
 subtract.grid(row=2, column=4)
-multiply = tk.Button(root, text="x")
+multiply = tk.Button(root, text="x", command=press_multiply)
 multiply.grid(row=3, column=4)
-divide = tk.Button(root, text="/")
+divide = tk.Button(root, text="/", command=press_divide)
 divide.grid(row=4, column=4)
 
-exponent = tk.Button(frame4, text="^")
+exponent = tk.Button(frame4, text="^", command=press_exponent)
 exponent.grid(row=5, column=1)
+
 sqrt = tk.Button(frame4, text="√")
 sqrt.grid(row=5, column=3)
 
-solve = tk.Button(root, text="=")
+solve = tk.Button(root, text="=", command=press_solve)
 solve.grid(row=5, column=4)
 
 
